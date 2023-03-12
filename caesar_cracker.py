5220d68a4a7e1ddece88e84b44e50b6820ae2c28
Konstantinos Chanioglou <konstantinosch@hotmail.co.uk>
2023-03-12 18:00:24 +0200

    Update caesar_cracker.py

--- caesar_cracker.py
+++ caesar_cracker.py
@@ -1,12 +1,3 @@
-'''
-This program can hack messages encrypted 
-with the Caesar cipher from the previous project, even 
-if you don’t know the key. There are only 26 
-possible keys for the Caesar cipher, so a computer can easily try all possible decryptions and display the results to the user. In cryptography, we call 
-this technique a brute-force attack.
-'''
-
-
 def encrypt(text,key):
     ans = ""
     for i in range(len(text)): # iterate over the given text
